83e13a7b93c9878b75f11e2975f7d6fefbc2d3c7
Upendra406 <bakthulaupendra@gmail.com>
2021-07-20 23:34:51 +0530

    intial commit

---
+++ src/main/java/com/example/demo/MyTest.java
@@ -0,0 +1,14 @@
+package com.example.demo;
+
+public class MyTest {
+
+	public static void main(String[] args) {
+
+		String s1 = new String("Hi..");
+		String s2 = new String("Hello..");
+		System.out.println(s1.equals(s2));
+		
+		
+	}
+
+}
